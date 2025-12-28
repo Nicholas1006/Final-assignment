5d9bc57fdb8450e0e7740815b1b548c2c0739898
Nicholas Horvat <horvat.nicholas@gmail.com>
2025-12-28 13:14:44 +0000

    Bring over skybox struct

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -180,6 +180,31 @@ GLuint createSimpleShader() {
     return program;
 }
 
+
+static GLuint LoadSkyBoxTexture(const char *texture_file_path) {
+    int w, h, channels;
+    uint8_t* img = stbi_load(texture_file_path, &w, &h, &channels, 3);
+    GLuint texture;
+    glGenTextures(1, &texture);  
+    glBindTexture(GL_TEXTURE_2D, texture);  
+
+    // To tile textures on a box, we set wrapping to repeat
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    if (img) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture " << texture_file_path << std::endl;
+    }
+    stbi_image_free(img);
+
+    return texture;
+}
+
 // ====================
 // TEST CUBE
 // ====================
@@ -673,7 +698,6 @@ struct MyBot {
     
             // Normalize the final angle
             visualAdditionalYaw = normalizeAngle(visualAdditionalYaw);
-            std::cout << "visualAdditionalYaw: " << visualAdditionalYaw << std::endl;
         }
         if (model.animations.empty() || animationObjects.empty()) return;
 		
@@ -1319,6 +1343,268 @@ struct SimpleGround {
         glDeleteProgram(shader);
     }
 };
+struct SkyBox {
+	glm::vec3 position;		// Position of the box 
+	glm::vec3 scale;		// Size of the box in each axis
+	
+	GLfloat vertex_buffer_data[72] = {
+    -1.0f, -1.0f, 1.0f, 
+    1.0f, -1.0f, 1.0f, 
+    1.0f, 1.0f, 1.0f, 
+    -1.0f, 1.0f, 1.0f, 
+    
+    1.0f, -1.0f, -1.0f, 
+    -1.0f, -1.0f, -1.0f, 
+    -1.0f, 1.0f, -1.0f, 
+    1.0f, 1.0f, -1.0f,
+    
+    1.0f, -1.0f, 1.0f, 
+    1.0f, -1.0f, -1.0f, 
+    1.0f, 1.0f, -1.0f, 
+    1.0f, 1.0f, 1.0f, 
+
+    -1.0f, -1.0f, -1.0f, 
+    -1.0f, -1.0f, 1.0f, 
+    -1.0f, 1.0f, 1.0f, 
+    -1.0f, 1.0f, -1.0f,
+
+    -1.0f, 1.0f, -1.0f, 
+    1.0f, 1.0f, -1.0f, 
+    1.0f, 1.0f, 1.0f, 
+    -1.0f, 1.0f, 1.0f, 
+
+    -1.0f, -1.0f, 1.0f, 
+    1.0f, -1.0f, 1.0f, 
+    1.0f, -1.0f, -1.0f, 
+    -1.0f, -1.0f, -1.0f, 
+};
+
+	GLuint index_buffer_data[36] = {
+    0, 2, 1,     
+    0, 3, 2, 
+    
+    4, 6, 5, 
+    4, 7, 6, 
+
+    8, 10, 9, 
+    8, 11, 10, 
+
+    12, 13, 14, 
+    12, 14, 15, 
+
+    16, 18, 17, 
+    16, 19, 18, 
+
+    20, 21, 22, 
+    20, 22, 23, 
+};
+
+    GLfloat color_buffer_data[72] = {
+		// Front, red
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		// Back, yellow
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+
+		// Left, green
+		0.0f, 1.0f, 0.0f, 
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		// Right, cyan
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+		0.0f, 1.0f, 1.0f, 
+
+		// Top, blue
+		0.0f, 0.0f, 1.0f, 
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+
+		// Bottom, magenta
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 
+		1.0f, 0.0f, 1.0f, 
+		1.0f, 0.0f, 1.0f,  
+	};
+
+    GLfloat uv_buffer_data[48] = {
+        // (+Z)
+        0.25f, 0.333333333333f,
+        0.5f, 0.333333333333f,
+        0.5f, 0.66666666667f,
+        0.25f, 0.66666666667f,
+
+        // (-Z)
+        0.75f, 0.333333333333f,
+        1.0f, 0.333333333333f,
+        1.0f, 0.66666666667f,
+        0.75f, 0.66666666667f,
+
+        // (+X)
+        0.5f, 0.333333333333f,
+        0.75f, 0.333333333333f,
+        0.75f, 0.66666666667f,
+        0.5f, 0.66666666667f,
+
+        // (-X)
+        0.0f, 0.333333333333f,
+        0.25f, 0.333333333333f,
+        0.25f, 0.66666666667f,
+        0.0f, 0.66666666667f,
+
+        // (-Y) -
+        0.25f, 1.0f,     
+        0.5f, 1.0f,     
+        0.5f, 0.66666666667f,   
+        0.25f, 0.66666666667f,  
+        
+        // +Y)
+        0.25f, 0.333333333333f,  
+        0.5f, 0.333333333333f,   
+        0.5f, 0.0f,     
+        0.25f, 0.0f,    
+    };
+    
+	// OpenGL buffers
+	GLuint vertexArrayID; 
+	GLuint vertexBufferID; 
+	GLuint indexBufferID; 
+	GLuint colorBufferID;
+	GLuint uvBufferID;
+	GLuint textureID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	GLuint textureSamplerID;
+	GLuint programID;
+
+	void initialize(glm::vec3 position, glm::vec3 scale) {
+		for (int i = 0; i < 72; ++i) color_buffer_data[i] = 1.0f;
+		// Define scale of the building geometry
+		this->position = position;
+		this->scale = scale;
+
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		// Create a vertex buffer object to store the vertex data		
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		/// Create a vertex buffer object to store the UV data
+		glGenBuffers(1, &uvBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data,GL_STATIC_DRAW);
+
+        // Create a vertex buffer object to store the color data
+        // TODO: 
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+		// Create an index buffer object to store the index data that defines triangle faces
+		glGenBuffers(1, &indexBufferID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_buffer_data), index_buffer_data, GL_STATIC_DRAW);
+
+		// Create and compile our GLSL program from the shaders
+		programID = LoadShadersFromFile("../lab2/box.vert", "../lab2/box.frag");
+		if (programID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(programID, "MVP");
+
+        // Load a texture 
+        //this->textureID = LoadSkyBoxTexture("../lab2/sky.png");
+        //this->textureID = LoadSkyBoxTexture("../lab2/sky_debug.png");
+        this->textureID = LoadSkyBoxTexture("../assignment1/model/sky/sky.png");
+        //this->textureID = LoadSkyBoxTexture("../lab2/studio_garden_debug.png");
+
+        // Get a handle for our "textureSampler" uniform
+		textureSamplerID = glGetUniformLocation(programID,"textureSampler");
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(programID);
+
+        glDepthMask(GL_FALSE);
+        glDisable(GL_CULL_FACE);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+
+		// Model transform 
+		// -----------------------
+        glm::mat4 modelMatrix = glm::mat4();    
+        // Scale the box along each axis to make it look like a building
+		modelMatrix = glm::translate(modelMatrix, position);
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(scale.x, scale.y, scale.z));
+        modelMatrix = glm::rotate(modelMatrix, glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        // -----------------------
+
+		// Set model-view-projection matrix
+		glm::mat4 mvp = cameraMatrix * modelMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+		// Enable UV buffer and texture sampler
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);
+		// Set textureSampler to use texture unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glUniform1i(textureSamplerID, 0); 
+
+		// Draw the box
+		glDrawElements(
+			GL_TRIANGLES,      // mode
+			36,    			   // number of indices
+			GL_UNSIGNED_INT,   // type
+			(void*)0           // element array buffer offset
+		);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+        //glDisableVertexAttribArray(2);
+
+		glDepthMask(GL_TRUE);
+		glEnable(GL_CULL_FACE);
+
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteBuffers(1, &indexBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		//glDeleteBuffers(1, &uvBufferID);
+		//glDeleteTextures(1, &textureID);
+		glDeleteProgram(programID);
+	}
+}; 
+
 
 static GLuint LoadTextureTileBox(const char *texture_file_path) {
     int w, h, channels;
@@ -1389,11 +1675,13 @@ int main(void) {
     // Initialize test objects
     TestCube testCube;
     SimpleGround ground;
+    SkyBox skybox;
     
     std::cout << "=== Initializing Third-Person View ===" << std::endl;
     testCube.initialize();
     ground.initialize();
     bot.initialize();
+    skybox.initialize(glm::vec3(0,0,0), glm::vec3(5000,5000,5000));
     
     // Update camera front based on initial camera angles
     updateCameraPosition();
@@ -1452,6 +1740,7 @@ int main(void) {
         glm::mat4 viewProj = projection * view;
 
         // Render test objects
+        skybox.render(viewProj);
         bot.render(viewProj);
         ground.render(viewProj);
         testCube.render(viewProj);
@@ -1484,6 +1773,7 @@ int main(void) {
 	bot.cleanup();
     testCube.cleanup();
     ground.cleanup();
+    skybox.cleanup();
 
     glfwTerminate();
     return 0;
